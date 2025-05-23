9bc995289c79082984c5ff91387b47bb0f8d06cf
mdurc <matthewdurcan@icloud.com>
2025-05-23 10:14:45 -0400

    Added parser snapshot testing with all the files I have right now, they all currently pass

---
+++ src/testing/test_parser.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+
+#include "../lexer/lexer.h"
+#include "../parser/parser.h"
+#include "../parser/visitor.h"
+#include "util.h"
+#include "vendor/ApprovalTests.hpp"
+
+std::string generate_parser_output(const std::string& input_filepath) {
+  std::stringstream ss;
+  Lexer lexer;
+  try {
+    const std::vector<Token>& tokens = lexer.tokenize(input_filepath);
+
+    SymTab symtab;
+    Parser parser;
+    try {
+      std::vector<AstPtr> ast = parser.parse_program(&symtab, tokens);
+      print_ast(ast, ss);
+    } catch (const std::exception& e) {
+      ss << "PARSER_EXCEPTION: " << e.what() << std::endl;
+    }
+  } catch (const std::exception& e) {
+    ss << "LEXER_EXCEPTION: " << e.what() << std::endl;
+  }
+
+  return rtrim(ss.str());
+}
+
+TEST(ParserTests, ParserVarDecl) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("./samples/var_decl.sn"));
+}
+
+TEST(ParserTests, ParserControlFlow) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("./samples/control_flow.sn"));
+}
+
+TEST(ParserTests, ParserFunctions) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("./samples/functions.sn"));
+}
+
+TEST(ParserTests, ParserStructs) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("./samples/structs.sn"));
+}
+
+TEST(ParserTests, ParserPointers) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("./samples/pointers.sn"));
+}
+
+TEST(ParserTests, ParserMemory) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("./samples/memory.sn"));
+}
+
+TEST(ParserTests, ParserAsmAndErrors) {
+  ApprovalTests::Approvals::verify(
+      generate_parser_output("samples/asm_and_errors.sn"));
+}
